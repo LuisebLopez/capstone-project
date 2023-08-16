56703fc7afed52b54508e851c649feaf4bd00e01
Luis Lopez <luise.lopez.erazo@gmail.com>
2023-08-16 01:43:56 -0600

    Structuring the html and working with the opg meta tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import './App.css';
+import logo from '../public/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      HomePage
-    </div>
+    <>
+      <header>
+        <div>
+          <img src={logo}></img>
+        </div>
+        <div>
+          <nav>
+            <ul>
+              <li><a>Home</a></li>
+              <li><a>About</a></li>
+              <li><a>Menu</a></li>
+              <li><a>Reservations</a></li>
+              <li><a>Order Online</a></li>
+              <li><a>Login</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main></main>
+      <footer>
+        <div>
+          <img src={logo}></img>
+        </div>
+        <div>
+          <ul>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Menu</a></li>
+            <li><a>Reservations</a></li>
+            <li><a>Order Online</a></li>
+            <li><a>Login</a></li>
+          </ul>
+          <ul>
+            <li><a>Address</a></li>
+            <li><a>Phone Number</a></li>
+            <li><a>Email</a></li>
+          </ul>
+          <ul>
+            <li><a>Instagram</a></li>
+            <li><a>Facebook</a></li>
+            <li><a>Twitter</a></li>
+            <li><a>Youtube</a></li>
+          </ul>
+        </div>
+      </footer>
+    </>
   );
 }
 
